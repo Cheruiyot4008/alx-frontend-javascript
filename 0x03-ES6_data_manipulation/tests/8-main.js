8253d584af3b6338e457244dfb343c7119bfe06f
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2024-07-02 08:56:39 +0300

    javascript manipulation

---
+++ 0x03-ES6_data_manipulation/tests/8-main.js
@@ -0,0 +1,63 @@
+import cleanSet from "./8-clean_set.js";
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+8-main.js [unix] (02:59 01/01/1970)                                                                                                                                                                                       0,1 All
+-- INSERT --
+
